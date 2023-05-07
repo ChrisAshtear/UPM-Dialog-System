99239699d8fbc1e04f552805ac7df36b9fba5739
ChrisAshtear <chrisashtear@gmail.com>
2023-05-07 17:01:34 -0400

    added better error handling for dialog files.

--- Runtime/DialogFile.cs
+++ Runtime/DialogFile.cs
@@ -32,26 +32,73 @@ public struct DialogFile
     //2nd constructor for using txt or json?
     public DialogFile(XElement element, string assetPath)
     {
+        string last_valid_id = "none";
+        string last_valid_line = "none";
         // Attributes
-        name = (string)element.Attribute("name");
-        description = (string)element.Attribute("description");
-        author = (string)element.Attribute("author");
-        path = assetPath;
-        unityPath = assetPath.Replace('\\', '/');
-        webglPath = (string)element.Attribute("webglpath");
-        portraitFolder = (string)element.Attribute("portraitFolder");
-        // Elements
-        events = element
-            .Elements("dialogList")
-            ?.Elements("dialog")
-            .Select(p => new DialogEvent
-            {
-                id = (string)p.Attribute("id"),
-                portrait = (string)p.Attribute("portrait"),
-                dialogPages = ((string)p.Attribute("text")).Split('|'),
-            })
-            .ToDictionary(o => o.id, o => o);
+        try
+        {
+            name = (string)element.Attribute("name");
+            description = (string)element.Attribute("description");
+            author = (string)element.Attribute("author");
+            path = assetPath;
+            unityPath = assetPath.Replace('\\', '/');
+            webglPath = (string)element.Attribute("webglpath");
+            portraitFolder = (string)element.Attribute("portraitFolder");
 
+            
+            // Elements
+            events = element
+                .Elements("dialogList")
+                ?.Elements("dialog")
+                .Select(p => ParseEvent(p))
+                .Distinct()
+                .ToDictionary(o => o.id, o => o);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+            // Get stack trace for the exception with source file information
+            name = null;
+            description = null;
+            author = null;
+            path = null;
+            unityPath = null;
+            webglPath = null;
+            portraitFolder = null;
+            events = null;
+        }
+    }
+
+    private static string[] ParseText(XAttribute text, string id)
+    {
+        string[] nullDialog = new string[] {$"NO DIALOG for ID {id}"};
+        if(text!= null)
+        {
+            return ((string)text).Split('|');
+        }
+        else {
+            Debug.LogWarning("Dialog: No dialog text found for " + id);
+            return nullDialog; 
+        }
+    }
+
+    public static DialogEvent ParseEvent(XElement a)
+    {
+        try
+        {
+            DialogEvent evt = new DialogEvent
+            {
+                id = (string)a.Attribute("id"),
+                portrait = (string)a.Attribute("portrait"),
+                dialogPages = ParseText(a.Attribute("text"), (string)a.Attribute("id")),
+            };
+            return evt;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Dialog: Cannot parse Line " + a.ToString());
+            return new DialogEvent { dialogPages = null, id = "bad", portrait = "none" };
+        }
     }
 
 
